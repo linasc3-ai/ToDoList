f997874a3ddc62e114682ed5ac046a2e82a6f6e1
Lina Chihoub <lina.chihoub@gmail.com>
2021-07-19 10:41:52 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Alia Vyanne on 7/19/21.
+//  Copyright © 2021 Alia Vyanne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
